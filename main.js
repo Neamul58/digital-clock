4eb92ed22350495f24d67d95c32755c9aeba67f1
Neamul-karim <neamulkarim1994@gmail.com>
2025-12-11 19:27:18 +0600

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-//
-//const time = document.getElementById("time");
-//const timeFormat = document.getElementById("time-format");
-//
-//document.addEventListener("DOMContentLoaded", () => {
-//    setInterval(showTime, 1000);
-//});
-//
-//const showTime = () => {
-//    let date = new Date();
-//    let hr = date.getHours();
-//    let min = date.getMinutes();
-//    let sec = date.getSeconds();
-//
-//    hr = hr<10 ? `0${hr}` : hr;
-//    min = min<10 ? `0${min}` : min;
-//    sec = sec<10 ? `0${sec}` : sec;
-//
-//    time.innerHTML = `${hr} : ${min} : ${sec}`;
-//    timeFormat.innerHTML = hr>12 ? "PM" : "AM";
-//}
-
 const time = document.getElementById("time");
 const timeFormat = document.getElementById("time-format");
 
@@ -33,7 +11,7 @@ const showTime = ()=> {
     let min = date.getMinutes();
     let sec = date.getSeconds();
     
-    hr = hr <10 ? `0${hr}` : hr;
+    hr = hr < 10 ? `0${hr}` : hr;
     min = min < 10 ? `0${min}` : min;
     sec = sec < 10 ? `0${sec}` : sec;
     
