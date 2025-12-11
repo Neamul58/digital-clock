7aa896aecbf6b8183ac3170c35e303b64c59935b
Neamul-karim <neamulkarim1994@gmail.com>
2025-12-11 19:11:41 +0600

    first commit

--- main.js
+++ main.js
@@ -1,64 +1,42 @@
-// const time = document.getElementById("time");
-// const timeFormat = document.getElementById("time-format");
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     setInterval(showTime, 1000);
-// });
-
-// const showTime = () => {
-//     let date = new Date();
-//     let hr = date.getHours();
-//     let min = date.getMinutes();
-//     let sec = date.getSeconds();
-
-//     hr = hr<10 ? `0${hr}` : hr;
-//     min = min<10 ? `0${min}` : min;
-//     sec = sec<10 ? `0${sec}` : sec;
-
-//     time.innerHTML = `${hr} : ${min} : ${sec}`;
-
-//     timeFormat.innerHTML = hr>12 ? "PM" : "AM";
-// }
-
-// const time = document.getElementById("time");
-// const timeFormat = document.getElementById("time-format");
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     setInterval(showTime, 1000);
-// });
-
-// const showTime = () => {
-//     let date = new Date();
-//     let hr = date.getHours();
-//     let min = date.getMinutes();
-//     let sec = date.getSeconds();
-
-//     hr = hr<10 ? `0${hr}` : hr;
-//     min = min<10 ? `0${min}` : min;
-//     sec = sec<10 ? `0${sec}` : sec;
-
-//     time.innerHTML = `${hr} : ${min} : ${sec}`;
-
-//     timeFormat.innerHTML = hr>12 ? "PM" : "AM";
-// }
+//
+//const time = document.getElementById("time");
+//const timeFormat = document.getElementById("time-format");
+//
+//document.addEventListener("DOMContentLoaded", () => {
+//    setInterval(showTime, 1000);
+//});
+//
+//const showTime = () => {
+//    let date = new Date();
+//    let hr = date.getHours();
+//    let min = date.getMinutes();
+//    let sec = date.getSeconds();
+//
+//    hr = hr<10 ? `0${hr}` : hr;
+//    min = min<10 ? `0${min}` : min;
+//    sec = sec<10 ? `0${sec}` : sec;
+//
+//    time.innerHTML = `${hr} : ${min} : ${sec}`;
+//    timeFormat.innerHTML = hr>12 ? "PM" : "AM";
+//}
 
 const time = document.getElementById("time");
 const timeFormat = document.getElementById("time-format");
 
-document.addEventListener("DOMContentLoaded", () => {
-    setInterval(showTime, 1000);
+document.addEventListener("DOMcontentLoaded", () => {
+   setInterval(showTime, 1000); 
 });
 
-const showTime = () => {
+const showTime = ()=> {
     let date = new Date();
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-
-    hr = hr<10 ? `0${hr}` : hr;
-    min = min<10 ? `0${min}` : min;
-    sec = sec<10 ? `0${sec}` : sec;
-
+    
+    hr = hr <10 ? `0${hr}` : hr;
+    min = min < 10 ? `0${min}` : min;
+    sec = sec < 10 ? `0${sec}` : sec;
+    
     time.innerHTML = `${hr} : ${min} : ${sec}`;
     timeFormat.innerHTML = hr>12 ? "PM" : "AM";
 }
